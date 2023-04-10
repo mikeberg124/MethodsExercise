cdf6bd37fed0a342dd750e2c98872c4b3a9ba23a
Michael Berg <mberg12@bu.edu>
2023-04-10 15:11:38 -0400

    completed exercise 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,8 +2,23 @@
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
+
+
+            Methods.Add(6,8);
+            Methods.Subtract(6,8);
+            Methods.Multiply(6,8);
+            Methods.Divide(6,8);
+
+
+
+
+
+
+            //Exercise 1
+
             Console.WriteLine("What is your name");
             string Name = Console.ReadLine();
 
@@ -15,6 +30,17 @@
 
             Console.WriteLine($"That a great {favAnimal}! What is your favorite band?");
             string favoriteBand = Console.ReadLine();
+              
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
